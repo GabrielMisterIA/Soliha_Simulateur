5d4f4dd77a708c78c27acd3e852c4c2860bd2ba5
GabrielMisterIA <gabriel.alis@mister-ia.com>
2025-04-08 15:37:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/home/HomePage';
+import RenoverLogementSimulateur from './components/simulateurs/RenoverLogementSimulateur';
+import RenoverCoproprieteSimulateur from './components/simulateurs/RenoverCoproprieteSimulateur';
+import AdapterLogementSimulateur from './components/simulateurs/AdapterLogementSimulateur';
+import LouerBienSimulateur from './components/simulateurs/LouerBienSimulateur';
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<h1>🚀 Hello via HashRouter</h1>} />
-        <Route path="*" element={<h2>404 😵</h2>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/renover-logement" element={<RenoverLogementSimulateur />} />
+        <Route path="/renover-copropriete" element={<RenoverCoproprieteSimulateur />} />
+        <Route path="/adapter-logement" element={<AdapterLogementSimulateur />} />
+        <Route path="/louer-bien" element={<LouerBienSimulateur />} />
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
