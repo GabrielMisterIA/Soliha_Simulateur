b154097d6945b85ef14e06803e0f5bb20130b95f
GabrielMisterIA <gabriel.alis@mister-ia.com>
2025-04-08 15:05:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/home/HomePage';
-import RenoverLogementSimulateur from './components/simulateurs/RenoverLogementSimulateur';
-import RenoverCoproprieteSimulateur from './components/simulateurs/RenoverCoproprieteSimulateur';
-import AdapterLogementSimulateur from './components/simulateurs/AdapterLogementSimulateur';
-import LouerBienSimulateur from './components/simulateurs/LouerBienSimulateur';
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/renover-logement" element={<RenoverLogementSimulateur />} />
-        <Route path="/renover-copropriete" element={<RenoverCoproprieteSimulateur />} />
-        <Route path="/adapter-logement" element={<AdapterLogementSimulateur />} />
-        <Route path="/louer-bien" element={<LouerBienSimulateur />} />
-      </Routes>
-    </Router>
+    <div style={{ padding: "2rem", fontSize: "2rem" }}>
+      HELLO WORLD 🚀
+    </div>
   );
 }
 
